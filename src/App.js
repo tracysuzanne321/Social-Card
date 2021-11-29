ba5f6b3dad2089ee94cdea124ccc8caca4db8b69
Tracy Smith <tracysuzanne321@gmail.com>
2021-11-29 15:38:22 +0000

    created app skellington

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Switch, Route } from 'react-router-dom';
+import { AppContext } from './AppContext';
+import React, { useState } from 'react';
+import Home from './pages/home';
+import Login from './pages/login';
+import SignUp from './pages/signup';
+import UserAdmin from './pages/useradmin';
+import UserProfile from './pages/userprofile';
 
 function App() {
-	return <div className="App">My Page</div>;
+	const [user, setUser] = useState({
+		username: '',
+		email: '',
+	});
+	return (
+		<AppContext.Provider value={{ user, setUser }}>
+			<BrowserRouter>
+				<Switch>
+					<Route path="/useradmin">
+						<UserAdmin />
+					</Route>
+					<Route path="/signup">
+						<SignUp />
+					</Route>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/userprofile">
+						<UserProfile />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</AppContext.Provider>
+	);
 }
 
 export default App;
