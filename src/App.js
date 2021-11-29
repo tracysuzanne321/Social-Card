da7885af43692ebcae868caf2c0e8c4d0ba2f646
Tracy Smith <tracysuzanne321@gmail.com>
2021-11-29 12:11:35 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+	return <div className="App">My Page</div>;
+}
+
+export default App;
