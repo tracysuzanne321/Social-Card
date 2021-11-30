63ddc90281dabbbe94cd3fedda491a3e0248daca
Tracy Smith <tracysuzanne321@gmail.com>
2021-11-30 23:20:54 +0000

    Created and styled home page and added mobile nav

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className="absolute bottom-0 bg-green-500 flex flex-col  py-6 text-white w-full">
+		<footer className="mt-8 bg-green-500 flex flex-col  py-6 text-white w-full">
 			<div className="ml-12">&copy; 2021 SocialCard.</div>
 		</footer>
 	);
