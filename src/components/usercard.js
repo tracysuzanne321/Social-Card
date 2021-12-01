76df97f482f80761ecbec74e3fc6470254e9b867
lou duncan <louisa.duncan44@outlook.com>
2021-12-01 14:48:54 +0000

    Imported React Icons

--- src/components/usercard.js
+++ src/components/usercard.js
@@ -1,32 +1,47 @@
+import { IoLogoFacebook } from "react-icons/io";
+import { IoLogoGithub } from "react-icons/io";
+import { TiSocialInstagramCircular } from "react-icons/ti";
+import { AiFillTwitterCircle } from "react-icons/ai";
+
+// npm install react-icons --save
+
 const UserCard = () => {
-	return (
-		<div className="cardAll">
-			<div className="cardLeft">
-				<img src="" alt=""></img>{' '}
-			</div>
-			<div className="cardRight">
-				<h3>Example User</h3>
-				<p>
-					Poke slow-carb mixtape knausgaard, typewriter street art gentrify
-					hammock starladder roathse. Craies vegan tousled etsy austin.
-				</p>
-				<ul>
-					<li>
-						<a href="">link1</a>
-					</li>
-					<li>
-						<a href="">link2</a>
-					</li>
-					<li>
-						<a href="">link3</a>
-					</li>
-					<li>
-						<a href="">link4</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+  return (
+    <div className="box-content h-2rem w-6rem border-rounded-md">
+      <div className="cardLeft">
+        <img src="" alt="" className=""></img>{" "}
+      </div>
+      <div className="cardRight">
+        <h3 className="text-black text-xl">Example User</h3>
+        <p className="text-grey-md">
+          Poke slow-carb mixtape knausgaard, typewriter street art gentrify
+          hammock starladder roathse. Craies vegan tousled etsy austin.
+        </p>
+        <ul>
+          <li classname="w-2 h-2">
+            <a href="">
+              <IoLogoFacebook />
+            </a>
+          </li>
+          <li classname="w-2 h-2">
+            <a href="">
+              <IoLogoGithub />
+            </a>
+          </li>
+          <li classname="w-2 h-2">
+            <a href="">
+              <TiSocialInstagramCircular />
+            </a>
+          </li>
+          <li classname="w-2 h-2">
+            <a href="">
+              <AiFillTwitterCircle />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default UserCard;
