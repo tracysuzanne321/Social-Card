e0af27773f2a9e9258b98e9ca71fe56853951cac
lou duncan <louisa.duncan44@outlook.com>
2021-12-01 17:08:39 +0000

    imported react-icons

--- src/components/usercard.js
+++ src/components/usercard.js
@@ -1,32 +1,45 @@
+import { IoLogoGithub } from "react-icons/io";
+import { IoLogoFacebook } from "react-icons/io";
+import { TiSocialInstagramCircular } from "react-icons/ti";
+import { AiFillTwitterCircle } from "react-icons/ai";
+// npm install react-icons --save //
 const UserCard = () => {
-	return (
-		<div className="cardAll">
-			<div className="cardLeft">
-				<img src="" alt=""></img>{' '}
-			</div>
-			<div className="cardRight">
-				<h3>Example User</h3>
-				<p>
-					Poke slow-carb mixtape knausgaard, typewriter street art gentrify
-					hammock starladder roathse. Craies vegan tousled etsy austin.
-				</p>
-				<ul>
-					<li>
-						<a href="">link1</a>
-					</li>
-					<li>
-						<a href="">link2</a>
-					</li>
-					<li>
-						<a href="">link3</a>
-					</li>
-					<li>
-						<a href="">link4</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+  return (
+    <div className="cardAll">
+      <div className="cardLeft">
+        <img src="" alt=""></img>
+      </div>
+      <div className="cardRight">
+        <h3>Example User</h3>
+        <p>
+          Poke slow-carb mixtape knausgaard, typewriter street art gentrify
+          hammock starladder roathse. Craies vegan tousled etsy austin.
+        </p>
+        <ul>
+          <li>
+            <a href="/">
+              <IoLogoFacebook />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <IoLogoGithub />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <TiSocialInstagramCircular />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <AiFillTwitterCircle />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default UserCard;
