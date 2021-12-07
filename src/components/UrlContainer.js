d9478c1fddbd6e6fb122a58e75e2b9ed2b6f3b35
Tracy Smith <tracysuzanne321@gmail.com>
2021-12-07 11:58:29 +0000

    Changed icon and added an alert box

--- src/components/UrlContainer.js
+++ src/components/UrlContainer.js
@@ -1,4 +1,4 @@
-import { FaShareSquare } from 'react-icons/fa';
+import { AiOutlineCopy } from 'react-icons/ai';
 
 export const UrlContainer = ({ username }) => {
 	const fullUrl = window.location.href;
@@ -9,12 +9,14 @@ export const UrlContainer = ({ username }) => {
 			<div className="text-green-500">{username}</div>
 			<div>
 				<button
+					className="flex"
 					title="Copy your card to clipboard"
 					alt="Copy to clipboard"
 					onClick={() => {
+						alert('URL Copied to Clipboard');
 						navigator.clipboard.writeText(fullUrl);
 					}}>
-					<FaShareSquare className="cursor-pointer ml-5 h-7 w-7 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 ... fill-current text-gray-400" />
+					<AiOutlineCopy className="cursor-pointer ml-1.5 h-7 w-7 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 ... fill-current text-gray" />
 				</button>
 			</div>
 		</div>
