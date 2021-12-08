fe5b6de0e1b40eb6ea8e7e925077b412bdf09c2d
Tracy Smith <tracysuzanne321@gmail.com>
2021-12-08 20:21:58 +0000

    css mobile

--- src/components/usercard.js
+++ src/components/usercard.js
@@ -11,15 +11,15 @@ const UserCard = ({
 	return (
 		<>
 			<div
-				className={`w-full flex md:flex-row flex-col lg:ml-10 max-h-96 max-w-2xl mb-10 rounded-2xl shadow-2xl ${
+				className={`w-full flex md:flex-row flex-col lg:ml-10 max-w-2xl mb-10 rounded-2xl shadow-2xl ${
 					animated ? 'animate-fade-in-down' : ''
 				}`}>
 				<img
-					className="md:w-1/3  md:rounded-l-2xl object-cover object-top rounded-t-2xl"
+					className="w-full md:w-1/3  md:rounded-l-2xl object-cover object-top rounded-t-2xl"
 					src={profileImageUrl}
 					alt=""
 				/>
-				<div className="md:w-2/3 flex-col flex-wrap px-4">
+				<div className="w-full md:w-2/3 flex-col flex-wrap px-4">
 					<p className="mb-2 text-3xl mt-6">{fullName}</p>
 					<p className="mb-2 text-lg font-bold">{jobTitle}</p>
 					<p className="mb-4 text-body text-justify">{bio}</p>
